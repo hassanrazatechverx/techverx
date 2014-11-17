f113d06d9653bc2c4e170597fc34642e86950509
hassan <hassan@hassan-HP.(none)>
2014-11-17 18:51:26 +0500

    updates

--- config/initializers/monologue.rb
+++ config/initializers/monologue.rb
@@ -1,15 +1,15 @@
 Monologue.config do |config|
-  config.site_name = "@jipiboily / Jean-Philippe Boily"
-  config.site_subtitle = "my own place online"
-  config.site_url = "http://jipiboily.com"
+  config.site_name = "Techverx"
+  config.site_subtitle = "Techverx - Excellence through Brilliance"
+  config.site_url = "http://techverx.com/techverx/"
 
-  config.meta_description = "This is my personal blog about Rails, Monologue, programming, etconfig..."
+  config.meta_description = "This is Blog for my Website Techverx"
   config.meta_keyword = "rails, programming, monologue, ruby"
 
   config.admin_force_ssl = false
   config.posts_per_page = 10
 
-  config.disqus_shortname = "jipiboily"
+  config.disqus_shortname = "techverx"
 
   # LOCALE
   config.twitter_locale = "en" # "fr"
@@ -20,18 +20,19 @@ Monologue.config do |config|
 
   # ANALYTICS
   # config.gauge_analytics_site_id = "YOUR COGE FROM GAUG.ES"
-  # config.google_analytics_id = "YOUR GA CODE"
+   config.google_analytics_id = "UA-56825318-1"
+  
 
-  config.sidebar = ["latest_posts", "latest_tweets", "categories", "tag_cloud"]
+  config.sidebar = ["latest_posts", "categories", "tags"]
 
 
   #SOCIAL
-  config.twitter_username = "jipiboily"
-  config.facebook_url = "https://www.facebook.com/jipiboily"
+  config.twitter_username = "https://twitter.com/Techverx"
+  config.facebook_url = "https://www.facebook.com/TechVerx"
   config.facebook_logo = 'logo.png'
-  config.google_plus_account_url = "https://plus.google.com/u/1/115273180419164295760/posts"
-  config.linkedin_url = "http://www.linkedin.com/in/jipiboily"
-  config.github_username = "jipiboily"
+  config.google_plus_account_url = "https://plus.google.com/"
+  config.linkedin_url = "https://www.linkedin.com/company/techverx"
+  config.github_username = "hassanrazatechverx"
   config.show_rss_icon = true
 
 end
